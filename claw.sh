f7081775b5678f86a04a17bd0c6832819230fe94
Vauth <122077080+Vauth@users.noreply.github.com>
2025-04-24 22:15:05 +0330

    Update claw.sh

--- claw.sh
+++ claw.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+# Clone the repository
 git clone https://github.com/Vauth/fastapi
-cd fastapi
+
+# Change directory to the cloned repository
+cd fastapi || exit
+
+# Install required Python packages
 pip install -r requirements.txt
+
+# Run the FastAPI application
 python fast.py
