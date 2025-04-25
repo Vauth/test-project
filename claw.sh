3d9f924b82fbdd766a7143516997ccc4085821f3
Vauth <122077080+Vauth@users.noreply.github.com>
2025-04-25 09:57:05 +0330

    Update claw.sh

--- claw.sh
+++ claw.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Remove cache
-rm -rf fastapi
+apt install curl
 
-# Clone the repository
-git clone https://github.com/Vauth/fastapi
-
-# Change directory to the cloned repository
-cd fastapi || exit
-
-# Install required Python packages
-pip3 install -r requirements.txt --break-system-packages
-
-# Run the FastAPI application
-python3 fast.py
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
