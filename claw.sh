9cf10afa68e84ba82aef89a3e44637c9194f8ffa
Vauth <122077080+Vauth@users.noreply.github.com>
2025-04-25 10:03:28 +0330

    Update claw.sh

--- claw.sh
+++ claw.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-apt update
+git clone https://github.com/adarsh-goel/Filestreambot-pro
 
-apt install curl -y
+cd Filestreambot-pro
 
-bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+virtualenv -p /usr/bin/python3 venv
+
+. ./venv/bin/activate
+
+pip3 install -r requirements.txt
+
+python3 -m Adarsh
