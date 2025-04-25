e8e62c6c86898b8238cba3b577dd0aecc960a9e5
Vauth <122077080+Vauth@users.noreply.github.com>
2025-04-25 10:25:47 +0330

    Update claw.sh

--- claw.sh
+++ claw.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/Aadhi000/File-To-Link
-
-cd File-To-Link
-
+git clone https://github.com/fyaz05/FileToLink
+cd FileToLink
 pip3 install -r requirements.txt --break-system-packages
-
-python3 -m Adarsh
+python3 -m Thunder
