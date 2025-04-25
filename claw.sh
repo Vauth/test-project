871d70917e04d6d2e141dd03cbf2dc8553180382
Vauth <122077080+Vauth@users.noreply.github.com>
2025-04-25 10:17:31 +0330

    Update claw.sh

--- claw.sh
+++ claw.sh
@@ -4,10 +4,6 @@ git clone https://github.com/adarsh-goel/Filestreambot-pro
 
 cd Filestreambot-pro
 
-virtualenv -p /usr/bin/python3 venv
-
-. ./venv/bin/activate
-
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt --break-system-packages
 
 python3 -m Adarsh
