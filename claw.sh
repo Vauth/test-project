f66e36d29ffaf7b9278bb90e4c717a65f354b5cc
Vauth <122077080+Vauth@users.noreply.github.com>
2025-04-26 12:10:45 +0330

    Update claw.sh

--- claw.sh
+++ claw.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-git clone https://github.com/CodeXBotz/File-Sharing-Bot
-cd File-Sharing-Bot
+git clone https://github.com/SpringsFern/TG-FileStreamBot
+cd TG-FileStreamBot
 pip3 install -r requirements.txt --break-system-packages
-pip3 install -U pyrogram --break-system-packages
-python3 main.py
+python3 -m WebStreamer
