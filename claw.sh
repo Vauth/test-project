1d7fe6a87fc3dd3745a033d60761e2aa1de194a5
Vauth <122077080+Vauth@users.noreply.github.com>
2025-04-26 12:03:14 +0330

    Update claw.sh

--- claw.sh
+++ claw.sh
@@ -3,4 +3,5 @@
 git clone https://github.com/CodeXBotz/File-Sharing-Bot
 cd File-Sharing-Bot
 pip3 install -r requirements.txt --break-system-packages
+pip3 install -U pyrogram --break-system-packages
 python3 main.py
