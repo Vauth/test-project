0b9437b3546560eeb0740483032d58305d830234
Vauth <122077080+Vauth@users.noreply.github.com>
2025-05-01 20:47:45 +0330

    Update claw.sh

--- claw.sh
+++ claw.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/anasty17/mirror-leech-telegram-bot
-cd mirror-leech-telegram-bot
+git clone https://github.com/viperadnan-git/google-drive-telegram-bot
+cd google-drive-telegram-bot
 pip3 install -r requirements.txt --break-system-packages
 python3 -m bot
