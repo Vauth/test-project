22d237ea7900381528460df05de34ead81bcefcf
Vauth <122077080+Vauth@users.noreply.github.com>
2025-05-01 20:57:37 +0330

    Update claw.sh

--- claw.sh
+++ claw.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/viperadnan-git/google-drive-telegram-bot
-cd google-drive-telegram-bot
+git clone https://github.com/kalanakt/All-Url-Uploader
+cd All-Url-Uploader
 pip3 install -r requirements.txt --break-system-packages
-python3 -m bot
+python3 bot.py
